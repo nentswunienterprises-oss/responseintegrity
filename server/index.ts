b5f84a08f30671d4943a5b7b6605257d673c1f0f
nentswunienterprises-oss <nentswunienterprises@gmail.com>
2026-09-11 15:45:34 +0200

    Register oral defense and readiness routes in server entrypoint

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes.ts";
 import { registerCapabilityEngineRoutes } from "./routes/capabilityEngine";
 import { registerCapabilityPracticalEvidenceRoutes } from "./routes/capabilityPracticalEvidence";
+import { registerCapabilityOralDefenseRoutes } from "./routes/capabilityOralDefense";
 import { setupAuth } from "./supabaseAuth";
 import cors from 'cors';
 
@@ -100,6 +101,7 @@ app.use((req, res, next) => {
   await setupAuth(app); // Ensure session/auth middleware is active before routes
   registerCapabilityEngineRoutes(app);
   registerCapabilityPracticalEvidenceRoutes(app);
+  registerCapabilityOralDefenseRoutes(app);
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
